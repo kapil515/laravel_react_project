193a569f05eb955096c6e7545cf135bd1f9a1108
kapil515 <kapilb.mdb@gmail.com>
2025-07-25 14:05:26 +0530

    chnge

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Welcome({ auth,products }) {
                 <Navbar auth={auth} />
                 <div className="relative flex min-h-screen flex-col items-center justify-start selection:bg-[#FF2D20] selection:text-white">
                     <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                        <Banner />
+                        <Banner/>
                         <Category />
                         <Products products={products} /> 
                         <header className="grid grid-cols-2 gap-2 py-10 lg:grid-cols-1 items-start">
