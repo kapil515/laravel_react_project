af9f2c19d9d4ec380eedc6ed126463d2271f0b76
anjummishra <anjummishra.mdb@gmail.com>
2025-08-06 17:52:22 +0530

    add

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 use App\Http\Controllers\Api\CheckoutController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/checkout', [CheckoutController::class, 'process']);
 Route::get('/stripe', [CheckoutController::class, 'showStripePage']);
 Route::post('/orders/{order}/pay', [CheckoutController::class, 'process']);
+
 
